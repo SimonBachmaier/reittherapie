3da3aadf6870ad69085e454df56d305c69fbe17c
SimonBachmaier <simon.bachmaier@freenet.de>
2020-11-28 17:40:50 +0100

    cleanup gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,18 +67,6 @@ module.exports = {
         theme_color: `#ffffff`,
         display: `standalone`,
         icon: "static/img/favicon/android-chrome-512x512.png",
-        // "icons":[
-        //   {
-        //     "src":"static/img/favicon/android-chrome-192x192.png",
-        //     "sizes":"192x192",
-        //     "type":"image/png"
-        //   },
-        //   {
-        //     "/src":"static/img/favicon/android-chrome-512x512.png",
-        //     "sizes":"512x512",
-        //     "type":"image/png"
-        //   }
-        // ],
       },
     },
     {
